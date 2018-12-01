cffd82d184ab980966b068a9027ff01e0e9d245b
Marco Sirabella <marco@sirabella.org>
2018-12-01 15:55:40 -0500

    Cleanup & tranisition from lists to hashtables for lookup

--- 1/first.scm
+++ 1/first.scm
@@ -7,7 +7,7 @@
 	(normalize-list (if (null? l) v
 		(apply f (car l) (reduce f (cdr l) v)))))
 
-(define (sum l)
+(define (part1 l)
 	(reduce + l))
 
 
@@ -15,19 +15,34 @@
 	(reverse (car (reverse (reduce (lambda (a b #!optional (c '())) (list (+ a b) (cons (+ a b) c))) (reverse l) args)))))
 
 
-(define l (map string->number (call-with-input-file "input" (lambda (p) (read-all p read-line)))))
+(define (set-member? k t)
+	(table-ref t k #f))
 
-(define (members l2 l)
+(define (set-add! t k)
+	(table-set! t k #t))
+
+(define (set-adds! t l)
+	(if (null? l)
+		t
+		(let ()
+			(set-add! t (car l))
+			(set-adds! t (cdr l)))))
+
+(define (set-members? l2 l)
 	(if (null? l2)
 		#f
-		(if (member (car l2) l)
+		(if (set-member? (car l2) l)
 			(car l2)
-			(members (cdr l2) l))))
+			(set-members? (cdr l2) l))))
 
-(define (test2 l #!optional (acc '(0)))
-	(let ((new (wrapper l (car (reverse acc)))))
-		(if (members new acc) 
-			(members new acc)
-			(test2 l (append acc (wrapper l (car (reverse acc))))))))
+(define (part2 l #!optional (acc (make-table)) (last 0))
+	(let ((new (wrapper l last)))
+		(if (set-members? new acc) 
+			(set-members? new acc)
+			(let ()
+				(part2 l (set-adds! acc new) (car (reverse new)))))))
 
-(println (test2 l))
+(define l (map string->number (call-with-input-file "input" (lambda (p) (read-all p read-line)))))
+
+(println (part1 l))
+(println (part2 l))
