fe89b025a53b96038270ee73362bd26684e933b1
Marco Sirabella <marco@sirabella.org>
2018-12-01 12:07:42 -0500

    Add optional argument, default starting val to 0

--- 1/first.scm
+++ 1/first.scm
@@ -1,13 +1,13 @@
 (define (sublist l start stop)
 	(vector->list (subvector (list->vector l) start stop)))
 
-(define (reduce f l v)
+(define (reduce f l #!optional (v 0))
 	(f (list-ref l (- (length l) 1))
 		(if (eq? (length l) 1)
 			v
 			(reduce f (sublist l 0 (- (length l) 1)) v))))
 
 (define (sum l)
-	(reduce + l 0))
+	(reduce + l))
 
 (print (sum (map string->number (call-with-input-file "input" (lambda (p) (read-all p read-line))))))
