6f6e5fe98f16a93bc5469c5fcaa8ae4f654be3e5
Marco Sirabella <marco@sirabella.org>
2018-12-01 12:10:11 -0500

    + takes variable arguments, is basically sum

--- 1/first.scm
+++ 1/first.scm
@@ -1,8 +1,4 @@
-(define (reduce f l #!optional (v 0))
-	(if (null? l) v
-		(f (car l) (reduce f (cdr l) v))))
-
 (define (sum l)
-	(reduce + l))
+	(apply + l))
 
 (print (sum (map string->number (call-with-input-file "input" (lambda (p) (read-all p read-line))))))
